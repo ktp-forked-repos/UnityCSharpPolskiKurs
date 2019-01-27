28cb05d27fefbeb5867354135b6b15ed97cedf94
Kamil <42280008+NuKamil@users.noreply.github.com>
2019-01-27 10:54:01 +0100

    Constructors Add

--- Constructors/Person.cs
+++ Constructors/Person.cs
@@ -23,10 +23,28 @@ namespace Constructors
             address = "Utheimsveien";
             age = 14;
         }
-
+        
         public Person(int id, string firstname, string lastname, string address, int age, string gender)
         {
-
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.age = age;
+            this.gender = gender;
+        }
+        public Person(int id, string firstname, string lastname)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            address = "Utheimsveien";
+            age = 18;
+        }
+        public Person(int id, string firstname)
+        {
+            this.id = id;
+            this.firstname = firstname;
         }
 
     }
--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -10,6 +10,10 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            Person person2 = new Person(1, "Tomek");
+            Person person3 = new Person(2, "Tomek", "Nowak");
+            Person person4 = new Person(2, "Tomek", "Nowak", "Katowice",18,"mężczyzna");
         }
     }
 }
