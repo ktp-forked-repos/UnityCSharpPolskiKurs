c6e17bee350c6caa121c06c4679d06041bf4596c
Kamil Nurkowski <kamill1983@gmail.com>
2019-01-28 21:01:56 +0100

    Immutable

--- Typy/Program.cs
+++ Typy/Program.cs
@@ -25,9 +25,13 @@ namespace Typy
         private static void Immutable()
         {
             string name = " Marcin ";
-            name.Trim();
+            name = name.Trim();
+
+            DateTime date = new DateTime(2015,3,10);
+            date = date.AddHours(48);
 
             Console.WriteLine(name);
+            Console.WriteLine(date);
         }
 
         private static void PassByValueAndRef()
