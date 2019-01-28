d652e49ab930afeaa05d4186fa16cf8c544c452d
Kamil Nurkowski <kamill1983@gmail.com>
2019-01-28 20:02:57 +0100

    Nowe TYpy

--- Typy/Program.cs
+++ Typy/Program.cs
@@ -4,9 +4,26 @@ namespace Typy
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void GiveName(ref Diary diary)
         {
             diary = new Diary();
+            Diary.Name = "Nowy dzienniczek Marcina";
+
         }
+        static void IncerementNumber(out int number)
+        {
+            number = 42;
+        }
+
+        public static void Main(string[] args)
+        {
+            Diary d1 = new Diary();
+            Diary d2 = d1;
+
+            GiveName((ref d2));
+            Console.WriteLine(d2.Name);
+        }
+      
+
     }
 }
