54e7ae79539bdf8384ec45a9d5bcb7c5d965cf66
Kamil Nurkowski <kamill1983@gmail.com>
2019-02-02 21:45:32 +0100

    Unit tests monodevelop

---
+++ TestDiary/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Typy.Tests
+{
+    [TestFixture()]
+    public class DiaryTests
+    {
+        [Test()]
+        public void CalculatemaxGrade()
+        {
+            Diary diary = new Diary();
+        }
+    }
+}
